d69f9fd25355340ef76268e49538775ce99b148d
deepaksharmadk <dks847203@gmail.com>
2024-08-23 14:31:17 +0530

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layouts/layout";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Layout> home page</Layout>} />
+        <Route path="*" element={<div> page not found</div>} />
+      </Routes>
     </>
   );
 }
