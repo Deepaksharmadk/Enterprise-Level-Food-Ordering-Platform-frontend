9492cb52a22223800d2cbfeed9287755c9027481
deepaksharmadk <dks847203@gmail.com>
2024-08-24 22:47:15 +0530

    added homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import HomePage from "./pages/HomePage";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout> home page</Layout>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              {" "}
+              <HomePage />{" "}
+            </Layout>
+          }
+        />
         <Route path="*" element={<div> page not found</div>} />
       </Routes>
     </>
