c8bbe55f288aa78aede275c3faf89689baefe67e
deepaksharmadk <dks847203@gmail.com>
2024-08-26 22:09:30 +0530

    completed auth login

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,14 +1,28 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+import UsernameMenu from "./UserNameMenu";
 
 const MainNav = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
     <span className="flex space-x-2 items-center">
-      <Button
-        variant="ghost"
-        className="font-bold hover:text-orange-500 hover:bg-white"
-      >
-        Log In
-      </Button>
+      {isAuthenticated ? (
+        <>
+          <Link to="/order-status" className="font-bold hover:text-orange-500">
+            Order Status
+          </Link>
+          <UsernameMenu />
+        </>
+      ) : (
+        <Button
+          onClick={async () => await loginWithRedirect()}
+          variant="ghost"
+          className="font-bold hover:text-orange-500 hover:bg-white"
+        >
+          Log In
+        </Button>
+      )}
     </span>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithNavigate from "./auth0/Auth0ProviderWithNavigate.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <App />
+      <Auth0ProviderWithNavigate>
+        <App />
+      </Auth0ProviderWithNavigate>
     </Router>
   </StrictMode>
 );
