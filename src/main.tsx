d551a56b0cdccef1e64ed970abe4fd323532bc5c
deepaksharmadk <dks847203@gmail.com>
2024-08-29 02:07:19 +0530

    add react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import Auth0ProviderWithNavigate from "./auth0/Auth0ProviderWithNavigate.tsx";
-
-createRoot(document.getElementById("root")!).render(
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Auth0ProviderWithNavigate from './auth0/Auth0ProviderWithNavigate.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
-        <App />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <App />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
-  </StrictMode>
+  </StrictMode>,
 );
