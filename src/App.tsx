b5e5acd97abec7ba0c3108c0cb55297fb608dd41
deepaksharmadk <dks847203@gmail.com>
2024-08-31 01:07:37 +0530

    setup toaster and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Layout from "./layouts/Layout";
-import HomePage from "./pages/HomePage";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import HomePage from './pages/HomePage';
+import AuthCallbackPage from './pages/AuthCallbackPage';
+import ProtectedRoute from './auth0/ProtectedRoute';
+import UserProfilePage from './pages/UserProfilePage';
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
         <Route
           path="/"
           element={
-            <Layout>
-              {" "}
-              <HomePage />{" "}
+            <Layout showHero>
+              <HomePage />
             </Layout>
           }
         />
+        <Route element={<ProtectedRoute />}>
+          <Route
+            path="/user-profile"
+            element={
+              <Layout>
+                <UserProfilePage />
+              </Layout>
+            }
+          />
+        </Route>
+
         <Route path="*" element={<div> page not found</div>} />
       </Routes>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Toaster } from 'sonner';
 import Auth0ProviderWithNavigate from './auth0/Auth0ProviderWithNavigate.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <Auth0ProviderWithNavigate>
           <App />
+          <Toaster visibleToasts={1} position="top-right" richColors />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
